0dba9701c77ad3e287e5c026547b1af33dac9163
jakubnecessary <cabbage1991@gmail.com>
2021-07-01 14:29:36 +0100

    Initial commit, kinda late

---
+++ models/event_list.py
@@ -0,0 +1,9 @@
+from models.event import *
+
+event_1 = Event("16th August 2021", "Connor's Birthday", 1, "Gamer Room", "Sad solo Birthday")
+event_2 = Event("22nd April 2021", "Jakub's Birthday", 42, "Bar", "Actually fun Birthday")
+
+events = [event_1, event_2]
+
+def add_new_event(event):
+    events.append(event)
